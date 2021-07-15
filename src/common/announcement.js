e8b5dcc9a131ec9708eb6268fa4f169208bdfe04
Elvis Offor <eoffor21@gmail.com>
2021-07-15 00:35:12 -0700

    Cleaning up unused code + comments

--- src/common/announcement.js
+++ src/common/announcement.js
@@ -15,39 +15,7 @@ const { Panel } = Collapse;
 class Announcement extends Component {
   constructor(props) {
     super(props)
-
-    this.messages = [
-      {
-        start_date: '2021-07-14',
-        exp_date: '2021-07-18',
-        title: 'This is a Title 1', 
-        body: 'Hello world.',
-      },
-      {
-        // start_date: 'YYYY-MM-DD',
-        // start_date: '2021-07-12',
-        start_date: '2021-07-14',
-        exp_date: '2021-07-14',
-        title: 'Sample Announcement Title',
-        body: `**This** is a markdown test using [react-markdown](https://github.com/remarkjs/react-markdown).`
-      },
-      {
-        start_date: '2021-07-11',
-        exp_date: '2021-07-13',
-        title: 'This is a Title 2', 
-        body: 'Hello world.',
-      },
-      {
-        start_date: '2021-06-22',
-        exp_date: '2021-07-10',
-        title: 'This is a Title 3', 
-        body: 'Hello world.',
-      },
-    ]
-
     this.state = {
-      // drawer_open: true,
-      // drawer_open: false,
       drawer_open: this.props.open,
       notification_data: null,
       latest_message: null,
@@ -92,22 +60,13 @@ class Announcement extends Component {
   }
 
   getLatestMessageIndex(message_list){
-    // if(this.state.notification_data){
-    console.log(message_list)
     if(message_list){
-      // let latest_message
       let latest_message_index
-      // latest_message = this.state.notification_data[0]
 
       // Set as latest mesage if current date is within bounds
-      // for( let data_index in this.state.notification_data ){
-      // for( let i = 0; i < this.state.notification_data.length; i++ ){
-        // const data = this.state.notification_data[i]
       for( let i = 0; i < message_list.length; i++ ){
         const data = message_list[i]
         if( this.displayMessage(data.start_date, data.exp_date) ){
-          // this.setState({latest_message_index: i})
-          // this.setState({latest_message_index: i})
           // latest_message = data
           latest_message_index = i
           break
@@ -185,74 +144,10 @@ class Announcement extends Component {
   }
 
   renderLatestMessage(){
-    // const latest_message = this.getLatestMessage()
-    const latest_message = this.state.latest_message
-    const empty_message =
-      <div style={{textAlign: "center"}}>
-        <Typography><Title level={4}>Announcements</Title></Typography>
-        <Empty
-          description={
-            <span>No recent notices</span>
-          }
-        >
-          <Button type="primary" size="middle" onClick={() => this.props.toggleDrawer(false)}>
-            Close
-          </Button>
-        </Empty>
-      </div>
-
-    // if(false){
-    // if(this.state.notification_data){
-    if(latest_message){
-      // const latest_message = this.state.notification_data[0]
-      const message_style = {
-        fontSize: 18,
-        // textAlign: "center",
-      }
-
-      let rendered_message
-      if(this.displayMessage(latest_message.start_date, latest_message.exp_date)){
-        rendered_message = 
-          <Row justify="center" align="middle" >
-            {/* <Row justify="center" align="middle" gutter={[0, 10]}> */}
-            <Col span={14} style={{textAlign: "center"}}>
-              <Typography>
-                {/* <Title level={3} style={{textAlign: "center"}}> */}
-                <Title level={3}>
-                  {latest_message.title}
-                </Title>
-                <Paragraph style={message_style}>
-                  <ReactMarkdown linkTarget="_blank">
-                    {latest_message.body}
-                  </ReactMarkdown>
-                </Paragraph>
-              </Typography>
-            </Col>
-            <Col span={14} style={{textAlign: "center"}}>
-              <Button type="primary" size="middle" onClick={() => this.props.toggleDrawer(false)}>
-                Close
-              </Button>
-            </Col>
-          </Row>
-      }else{
-        rendered_message = empty_message
-      }
-      return rendered_message
-    }
-    else{
-      return empty_message
-    }
-  }
-
-  renderLatestMessage2(){
     const latest_message = this.state.latest_message
     console.log(latest_message)
     const message_style = {
       fontSize: 20,
-      // textAlign: "center",
-      // paddingLeft: "20%",
-      // paddingRight: "20%",
-      // maxWidth: "75%"
     }
 
     if(this.displayMessage(latest_message.start_date, latest_message.exp_date)){
@@ -304,8 +199,6 @@ class Announcement extends Component {
     // const remaining_messages = this.getRemainingMessages()
     const remaining_messages = this.state.remaining_messages
     if( remaining_messages ){
-    // if( this.state.notification_data ){
-      // const remaining_messages = this.state.notification_data.slice(1)
       const message_style = {
         fontSize: 17,
         // paddingTop: 10,
@@ -365,29 +258,11 @@ class Announcement extends Component {
   }
 
   render() {
-    let button_style = {
-      // position: 'absolute',
-      // left: 100,
-      // bottom: 100,
-      // width: 300,
-      // height: 300,
-      // zIndex: 20,
-    }
-
-    let message_style = {
-      fontSize: 20,
-      // textAlign: "center",
-      // paddingLeft: "20%",
-      // paddingRight: "20%",
-      // maxWidth: "75%"
-    }
-
-
     let announcement_contents
     if(this.state.notification_data && this.state.latest_message){
       announcement_contents =
         <div>
-          {this.renderLatestMessage2()}
+          {this.renderLatestMessage()}
           {this.renderRemainingMessages()}
         </div>
     }
@@ -399,14 +274,10 @@ class Announcement extends Component {
       <div>
         <Drawer
           // title="Announcements"
-          // visible={this.state.drawer_open}
           visible={this.props.open}
           onClose={() => this.props.toggleDrawer(false)}
           placement="bottom"
           height={265}
-          style={{
-            // textAlign: "center"
-          }}
         >
           {announcement_contents}
         </Drawer>
