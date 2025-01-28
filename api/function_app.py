df255b75ab501553233eb60aaa4a6f1ba83da626
Ethan Holz <ethan.holz@omsf.io>
2025-01-28 14:45:41 -0700

    fix: cleanup to match the AWS implementation

--- api/function_app.py
+++ api/function_app.py
@@ -10,15 +10,14 @@ from random import choices
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-def generate_sas_url(filename: str, job_id: str):
+def generate_sas_url(filename: str, job_id: str, formatted_date: str):
     now = datetime.now(timezone.utc)
     expiry = now + timedelta(hours=1)
     formatted_expiry = expiry.strftime("%Y-%m-%dT%H:%M:%SZ")
     account_name = os.environ["STORAGE_ACCOUNT_NAME"]
     account_key = os.environ["STORAGE_ACCOUNT_KEY"]
     container_name = os.environ["STORAGE_CONTAINER_NAME"]
-    blob_name = f"{job_id}/{filename}"
-    # TODO: validate that this works as we expect.
+    blob_name = f"{formatted_date}/{job_id}/{filename}"
     sas = generate_container_sas(
         account_name=account_name,
         container_name=container_name,
@@ -27,14 +26,6 @@ def generate_sas_url(filename: str, job_id: str):
         permission=ContainerSasPermissions(write=True),
         expiry=expiry,
     )
-    # sas = generate_blob_sas(
-    #     account_name=account_name,
-    #     container_name=container_name,
-    #     blob_name=blob_name,
-    #     account_key=account_key,
-    #     permission=BlobSasPermissions(write=True),
-    #     expiry=expiry,
-    # )
     upload_url = f"https://{account_name}.blob.core.windows.net/{container_name}/{blob_name}?{sas}"
     return {
         "upload_url": upload_url,
@@ -49,30 +40,22 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     try:
         req_body = req.get_json()
         file_list = req_body.get("file_list")
+        if not file_list:
+            return func.HttpResponse(
+                json.dumps({"error": "file_list is required"}),
+                mimetype="application/json",
+                status_code=400,
+            )
         job_id = "".join(choices(digits + ascii_lowercase, k=8))
+        date = datetime.now().strftime("%Y-%m-%d")
         urls = {}
         for file in file_list:
-            urls[file] = generate_sas_url(file, job_id)
-        # TODO: ensure that this matches correctly with the frontend
+            urls[file] = generate_sas_url(file, job_id, date)
         return_val = {
             "job_id": job_id,
             "urls": urls,
-            "date": datetime.now().strftime("%Y-%m-%d"),
+            "date": date,
         }
-        # if not file_name:
-        #     return func.HttpResponse(
-        #         json.dumps({"error": "file_name is required"}),
-        #         mimetype="application/json",
-        #         status_code=400,
-        #     )
-        # sas = generate_blob_sas(
-        #     account_name=account_name,
-        #     container_name=container_name,
-        #     blob_name=blob_name,
-        #     account_key=account_key,
-        #     permission=BlobSasPermissions(write=True),
-        #     expiry=expiry,
-        # )
         return func.HttpResponse(
             body=json.dumps(return_val),
             mimetype="application/json",
